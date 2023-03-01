1a7607c7bf9edb9f1bd0d031436071f2d5d10988
marinaLoBra <marina.lopezbravo@gmail.com>
2023-03-01 21:10:07 +0100

    Creación archivo variables

---
+++ 01_Variables.py
@@ -0,0 +1,17 @@
+#  Variables
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+
+my_int_variable = 8
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_variable))
+
+my_bool_variable = True
+print(my_bool_variable)
+
+print(my_string_variable, my_int_variable , my_bool_variable)
+print(my_string_variable,',',my_int_variable,',',my_bool_variable)
